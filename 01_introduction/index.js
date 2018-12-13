c8cd64b94204c7b5f4490482678501ee0b5a6171
mizogami <tomochuri25@gmail.com>
2018-12-14 05:15:14 +0900

    01: Handling User Input

--- 01_introduction/index.js
+++ 01_introduction/index.js
@@ -29,3 +29,22 @@ var app4 = new Vue({
     ]
   }
 })
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello 吉野家!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello 松屋!'
+  }
+})
