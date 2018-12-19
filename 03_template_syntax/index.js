d8d1563a61b3b89c7ccf2fd2853e1f038659b2ec
mizogami <tomochuri25@gmail.com>
2018-12-20 00:47:18 +0900

    03: Interpolations
    - Text
    - Raw HTML
    - Attributes
    - Using JavaScript Expressions

---
+++ 03_template_syntax/index.js
@@ -0,0 +1,10 @@
+var vm = new Vue({
+  el: '#app',
+  data: {
+    a: 1,
+    msg: 'Hello',
+    rawHtml: '<span style="color:red">This should be red.</span>',
+    isButtonDisabled: true,
+    ok: true
+  }
+})
