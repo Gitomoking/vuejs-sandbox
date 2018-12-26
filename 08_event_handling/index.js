a4b3e52c946ec3e3acb4dc84a56661d8ee6f5aa7
mizogami <tomochuri25@gmail.com>
2018-12-26 12:38:22 +0900

    08: v-on

---
+++ 08_event_handling/index.js
@@ -0,0 +1,21 @@
+var example1 = new Vue({
+  el: '#example-1',
+  data: {
+    counter: 0
+  }
+})
+
+var example2 = new Vue({
+  el: '#example-2',
+  data: {
+    name: 'Vue.js'
+  },
+  methods: {
+    greet: function (event) {
+      alert('Hello ' + this.name + '!')
+      if (event) {
+        alert(event.target.tagName)
+      }
+    }
+  }
+})
