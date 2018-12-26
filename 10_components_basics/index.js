678146e363727173cf097585c8b95b25d30d1f36
mizogami <tomochuri25@gmail.com>
2018-12-26 14:05:16 +0900

    10: Basics of Components

---
+++ 10_components_basics/index.js
@@ -0,0 +1,29 @@
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
+Vue.component('blog-post', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <div v-html="post.content"></div>
+    </div>
+  `
+})
+
+new Vue({
+  el: '#components-demo',
+  data: {
+    posts: [
+      { id: 1, title: 'My journey with Vue', content: 'a' },
+      { id: 2, title: 'Blogging with Vue', content: 'b' },
+      { id: 3, title: 'Why Vue is so fun', content: 'c' }
+    ]
+  }
+})
