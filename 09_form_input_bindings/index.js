02a003db02ee099590bdd26f431758337e62c90a
mizogami <tomochuri25@gmail.com>
2018-12-26 13:41:04 +0900

    09: input

---
+++ 09_form_input_bindings/index.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#example-3',
+  data: {
+    message: '',
+    ml_msg: '',
+    checkedNames: [],
+    picked: '',
+    selected: '',
+    d_selected: 'A',
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' },
+    ]
+  }
+})
